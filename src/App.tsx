8785361eb408373e667252672e84d651d14638d5
Ilya Dukhin <idukhini@mail.ru>
2024-03-09 14:01:49 +0300

    Add Appbar. Normal components positions

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { useReducer } from 'react';
+import { useCallback, useReducer, useState } from 'react';
 import './App.css';
 import Graph from './Graph';
 import { convertMousePosToSVGPos, generateEdgeId, generateNodeId } from './utils';
 import { GraphInfo, NodeType, EdgeType } from './types';
+import { AppBar, Box, Button, FormGroup, Grid, InputLabel, MenuItem, Select, Stack, TextField, Toolbar, ownerWindow } from '@mui/material';
 
 function reducer(state: GraphInfo, action: {type:string, args: any}):GraphInfo{
 
@@ -21,10 +22,11 @@ function reducer(state: GraphInfo, action: {type:string, args: any}):GraphInfo{
 				nodes: [...new_nodes]
 			};
 		case 'node-select':
+			const new_id = state.selectedNodeId == action.args.nodeId ? -1: action.args.nodeId;
 
 			return {
 				...state,
-				selectedNodeId: state.selectedNodeId == action.args.nodeId ? -1: action.args.nodeId
+				selectedNodeId: new_id
 			}
 		case 'node-create':
 			var [x, y] = convertMousePosToSVGPos(action.args.mousePosition.x, action.args.mousePosition.y);
@@ -51,6 +53,18 @@ function reducer(state: GraphInfo, action: {type:string, args: any}):GraphInfo{
 				...state,
 				edges: new_edges
 			}
+		case "selected-node-label-changed":
+			const new_value = (action.args.event.target as HTMLInputElement).value
+			const node1 = state.nodes.find((v) => {return v.id == state.selectedNodeId})
+			
+			const new_nodes1 = state.nodes.filter((node) => {return node.id !== state.selectedNodeId});
+			node1!.label = new_value;
+			new_nodes1.push(node1!)
+
+			return {
+				...state,
+				nodes: [...new_nodes1]
+			};
 	}
 
 	return state;
@@ -59,11 +73,33 @@ function reducer(state: GraphInfo, action: {type:string, args: any}):GraphInfo{
 function App() {
 	const [graphInfo, dispatch] = useReducer(reducer, {selectedNodeId: -1, nodes: [], edges: []})
 
+	const getSelectedNode = useCallback(() : NodeType => {
+		const emptyNode :NodeType = {x:0, y:0, label:"", id:-1};
+		if(graphInfo.selectedNodeId == -1) return emptyNode;
+		return graphInfo.nodes.find(v => v.id == graphInfo.selectedNodeId)!
+	}, [graphInfo])
+
 	return (
-		<div className="App">
-			<Graph graphInfo={graphInfo} dispatcher={dispatch} width={400} height={400}/>
-		</div>
+		<Box>
+
+			<AppBar position='fixed'>
+				<Toolbar>
+					<Button color={"inherit"}>NEW</Button>
+				</Toolbar>
+			</AppBar>
+			<Toolbar/>
+
+			<Grid container>
+				<Grid xs={9}>
+					<Graph graphInfo={graphInfo} dispatcher={dispatch}/>
+				</Grid>
+				<Grid item xs>
+					<TextField fullWidth label="Выбранная вершина" value={getSelectedNode().label} onChange={event => dispatch({type: 'selected-node-label-changed', args: {event: event}})}></TextField>
+				</Grid>
+			</Grid>
+
+		</Box>
 	);
 }
-
+{/* <Graph graphInfo={graphInfo} dispatcher={dispatch}/> */}
 export default App;
